b2c1750bb43b0a78089197878d8cd1f700b8c258
KimGranaderos <kchangranaderos@gmail.com>
2020-06-14 03:24:17 +0800

    Dynamic Websites With Jinja2 - Python and Flask #2

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    title = "Kim Granaderos' Blog"
+    return render_template("index.html", title=title)
 
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    title = "About Kim Granaderos"
+    names = ["John", "Mary", "Wes", "Sally"]
+    return render_template("about.html", names=names, title=title)
