a12c0ed9f9a2bd650b496e42ce8fb7535eeaafdb
KimGranaderos <kchangranaderos@gmail.com>
2020-06-14 06:21:17 +0800

    Web Forms With Flask - Python and Flask #4

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+subscribers = []
 
 
 @app.route('/')
@@ -14,3 +16,19 @@ def about():
     title = "About Kim Granaderos"
     names = ["John", "Mary", "Wes", "Sally"]
     return render_template("about.html", names=names, title=title)
+
+
+@app.route('/subscribe')
+def subscribe():
+    title = "Subscribe to my Email Newsletter"
+    return render_template("subscribe.html", title=title)
+
+
+@app.route('/form', methods=["POST"])
+def form():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+    subscribers.append(f"{first_name} {last_name} | {email}")
+    title = "Thank You!"
+    return render_template("form.html", title=title, subscribers=subscribers)
