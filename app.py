e3a01a62810cb24cb8273a870bbfdd3a50d7d520
KimGranaderos <kchangranaderos@gmail.com>
2020-06-15 04:34:03 +0800

    Using Databases With Flask - Python and Flask #8

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import smtplib
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+
+# Create db model
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create a function to return a string when we add something
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
 
 subscribers = []
+
+
+@app.route('/friends', methods=['POST', 'GET'])
+def friends():
+    title = "My Friend List"
+
+    if request.method == "POST":
+        name = request.form['name']
+        new_friend = Friends(name=name)
+
+        # Push to Database
+        try:
+            db.session.add(new_friend)
+            db.session.commit()
+            return redirect('/friends')
+        except:
+            return "There was an error adding your friend..."
+
+    else:
+        friends = Friends.query.order_by(Friends.date_created)
+        return render_template("friends.html", title=title, friends=friends)
 
 
 @app.route('/')
